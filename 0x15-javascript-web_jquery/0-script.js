dd05d7bc2a3cf56ae585bf3608df0cda48830be9
Kenneth Afegbai <kennethafegbai@gmail.com>
2023-05-03 22:30:30 +0100

    add  0x15-javascript-web_jquery tasks

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Javascript script that updates the text color of the <header> element to red (#FF0000)
+
+const headerColor = document.querySelector('header');
+headerColor.style.color = '#FF0000';
+
